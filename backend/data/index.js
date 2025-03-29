4288a0b5413ecb311737a505be16e93399dcd637
AlanAtrach1 <aatrach@stevens.edu>
2025-03-29 15:45:37 -0400

    file cleanup, added index

---
+++ backend/data/index.js
@@ -0,0 +1,9 @@
+import userDataFunctions from './userFunctions.js';
+import userFinancialDataFunctions from './userFinancialFunctions.js';
+import investmentDataFunctions from './investmentFunctions.js';
+import subInvestmentDataFunctions from './subInvestmentFunctions.js';
+
+export const userData = userDataFunctions;
+export const userFinancialData = userFinancialDataFunctions;
+export const investmentData = investmentDataFunctions;
+export const subInvestmentData = subInvestmentDataFunctions;
