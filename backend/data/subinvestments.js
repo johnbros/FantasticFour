8ab5ebfca700110fbcc604d29f2776512046d6c4
johnbros <jbrosnih@stevens.edu>
2025-03-29 17:36:19 -0400

    IDEK tbh

--- backend/data/subinvestments.js
+++ backend/data/subinvestments.js
@@ -1,7 +1,44 @@
 import { ObjectId } from "mongodb";
+import { subInvestments, investments } from "../config/mongoCollections.js";
+import validation from "../validation.js";
 
 const exportedMethods = {
+    async getSubInvestmentById(id) {
+        id = validation.checkId(id);
+        const subInvestmentCollection = await subInvestments();
+        const subInvestment = await subInvestmentCollection.findOne({ _id: ObjectId(id) });
+        if (!subInvestment) throw "Sub-investment not found";
+        return subInvestment;
+    },
 
+
+    // Add a su
+    async addSubInvestment(investmentId, subInvestmentName, totalValue) {
+        investmentId = validation.checkId(investmentId);
+        totalValue = validation.checkNum(totalValue);
+
+        const subInvestmentCollection = await subInvestments();
+
+        const newSubInvestment = {
+            investmentId,
+            totalValue,
+            investmentPercentage,
+            dateInvested: new Date(),
+        };
+
+        const insertInfo = await subInvestmentCollection.insertOne(newSubInvestment);
+        if (insertInfo.insertedCount === 0) throw "Could not add sub-investment";
+        return await this.getSubInvestmentById(insertInfo.insertedId.toString());
+    },
+
+    async removeSubInvestment(id) {
+        const subInvestmentCollection = await subInvestments();
+        const deletionInfo = await subInvestmentCollection.findOneAndDelete({
+            _id: new ObjectId(id),
+          });
+          if (!deletionInfo) throw `Could not delete post with id of ${id}`;
+          return { ...deletionInfo, deleted: true };
+    }
 };
 
 export default exportedMethods;
--- backend/data/userfinancials.js
+++ backend/data/userfinancials.js
@@ -1,7 +1,7 @@
 import { ObjectId } from "mongodb";
 import {getUserById} from "./users.js";
 import validation from "../validation.js";
-import { userFinancials } from "../config/mongoCollections.js";
+import { userFinancials, investments } from "../config/mongoCollections.js";
 
 const exportedMethods = {
     async getUserFinancialsById(id) {
@@ -12,31 +12,41 @@ const exportedMethods = {
         return userFinance;
     },
 
-    async addUserFinacials(id, riskTolerance, investmentAmount) {
+    async createUserFinacials(riskTolerance, investmentAmount) {
         const userFinance = await userFinancials();
-        id = validation.checkId(id);
         riskTolerance = validation.checkRiskTolerance(riskTolerance);
         if (!investmentAmount || typeof investmentAmount !== 'number' || isNaN(investmentAmount) || investmentAmount < 0) {
             throw "Investment amount must be a valid positive number";
         }
         const user = await getUserById(id);
         if (!user) throw "User not found";
+        const userFinancials = await userFinance.findOne({ _id: ObjectId(id) });
+        if (userFinancials) throw "User financials already exist";
+
 
         let portfolioValue = 0;
         let investments = []
 
         const newUserFinancial = {
-            id,
             portfolioValue,
             riskTolerance,
             investments,
         };
+        
         const insertInfo = await userFinance.insertOne(newUserFinancial);
-        if (insertInfo.insertedCount === 0) throw "Could not add user";
-        return id;
+        if (insertInfo.insertedCount === 0) throw "Could not add user financials";
+
+        const updatedUser = await userCollection.findOneAndUpdate(
+            { _id: ObjectId(id) },
+            { $set: { userFinancialID: insertInfo.insertedId } },
+            { returnDocument: "after" }
+        );
+        if (!updatedUser) throw "Could not update user with financials";
+
+        return updatedUser;
     },
 
-
+    //Update users total portfolio value
     async updatePortfolioValue(id, valueChange) {
         id = validation.checkId(id);
         if (!valueChange || typeof valueChange !== 'number' || isNaN(valueChange)) {
@@ -53,8 +63,47 @@ const exportedMethods = {
             { returnDocument: "after" }
         );
         if (!updatedUserFinance) throw "Could not update portfolio value";
-        return updatedUserFinance.value;
+        return user;
     },
+
+
+    //Add investment to user
+    async addInvestmentToUser(id, investmentId) {
+        id = validation.checkId(id);
+        investmentId = validation.checkId(investmentId);
+
+        // Check if the user exists
+        const userFinanceCollection = await userFinancials();
+        const investmentCollection = await investments();
+
+        // Get the user and investment information for the new investment
+        const investmentInfo = await investmentCollection.getInvestmentById(investmentId);
+        if (!investmentInfo) throw "Investment not found";
+        
+        
+        
+        // Check if the user financial collection exists (it should if the user exists)
+        const financialInfo = await userFinanceCollection.findOne({ _id: ObjectId(id) });
+        if (!financialInfo) throw "User financials not found";
+
+        const user = await this.updatePortfolioValue(id, investmentInfo.totalValue);
+        if (!user) throw "Problem updating Portfolio Value";
+
+        // List of investment IDs for the user
+        const investmentIds = financialInfo.investments;
+        
+        if (investmentIds.includes(investmentId)) throw "Investment already added to user";
+        investmentIds.add(investmentId);
+
+        const updatedUserFinance = await userFinanceCollection.findOneAndUpdate(
+            { _id: ObjectId(id) },
+            { $push: { investments: investmentIds } },
+            { returnDocument: "after" }
+        );
+
+        if (!updatedUserFinance) throw "Could not add investment to user";
+        return user;
+    }
 };
 
 export default exportedMethods;
