b9770acd76979503095a2f384f0fe2d97699b34d
johnbros <jbrosnih@stevens.edu>
2025-03-29 17:44:53 -0400

    wassup gang

--- backend/data/subinvestments.js
+++ backend/data/subinvestments.js
@@ -12,22 +12,36 @@ const exportedMethods = {
     },
 
 
-    // Add a su
+    // Add a sub investment
     async addSubInvestment(investmentId, subInvestmentName, totalValue) {
         investmentId = validation.checkId(investmentId);
         totalValue = validation.checkNum(totalValue);
 
         const subInvestmentCollection = await subInvestments();
+        const investmentCollection = await investments();
+        const investmentsInfo = await investmentCollection.getInvestmentById(investmentId);
+        if (!investmentsInfo) throw "Investment not found";
+        const investmentPercentage = (totalValue / (investmentsInfo.totalValue + totalValue)) * 100;
+        if (investmentPercentage > 100) throw "Investment percentage cannot exceed 100%";
 
         const newSubInvestment = {
             investmentId,
             totalValue,
+            subInvestmentName,
             investmentPercentage,
             dateInvested: new Date(),
         };
 
         const insertInfo = await subInvestmentCollection.insertOne(newSubInvestment);
         if (insertInfo.insertedCount === 0) throw "Could not add sub-investment";
+
+        const updatedInvestment = await investmentCollection.findOneAndUpdate(
+            { _id: ObjectId(investmentId) },
+            { $push: { subInvestments: investmentsInfo.add(insertInfo.insertedId) } },
+            { returnDocument: "after" }
+        );
+        if (!updatedInvestment) throw "Could not update investment with sub-investment";
+
         return await this.getSubInvestmentById(insertInfo.insertedId.toString());
     },
 
